d67ff2f58552248b01d51be858d1672cf16d81c2
garfr <gavratcliff@gmail.com>
2021-04-24 19:19:23 -0500

    chore: Fixed formatting

--- basename/basename.go
+++ basename/basename.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "os"
-  "strings"
-  "fmt"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("\033[31mbasename\033[0m: missing operand\n")
-    os.Exit(1)
-  }
+	if len(os.Args) < 2 {
+		fmt.Println("\033[31mbasename\033[0m: missing operand\n")
+		os.Exit(1)
+	}
 
-  res := strings.Split(os.Args[1], "/")
-  fmt.Println(res[len(res)-1])
+	res := strings.Split(os.Args[1], "/")
+	fmt.Println(res[len(res)-1])
 }
