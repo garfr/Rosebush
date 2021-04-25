054cb316ad7ec1d8ee42b5dd3218f9490bc60034
garfr <43555964+garfr@users.noreply.github.com>
2021-04-24 20:01:41 -0500

    feat: Added basename util (#3)
    * build: Added basic makefile
    
    * chore: Add basename to make
    
    * feat: add basename util
    
    * chore: Fixed formatting
    
    * chore: Added space between binary op

---
+++ basename/basename.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("\033[31mbasename\033[0m: missing operand\n")
+		os.Exit(1)
+	}
+
+	res := strings.Split(os.Args[1], "/")
+	fmt.Println(res[len(res) - 1])
+}
